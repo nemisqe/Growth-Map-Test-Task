776843dda073b72faa8c6646cb3152b586a4942f
Sergey Bosak <sergey.b@atomicactions.io>
2025-11-26 14:23:01 +0300

    nit

--- src/components/BaseScreen/BaseScreen.tsx
+++ src/components/BaseScreen/BaseScreen.tsx
@@ -1,8 +1,8 @@
-import { SafeAreaView } from "react-native-safe-area-context";
-import {createStyles} from './BaseScreen.styles';
+import { ScreenHeader } from "@components/ScreenHeader";
 import { useTheme } from "@utils/theme";
 import { useMemo } from "react";
-import { ScreenHeader } from "@components/ScreenHeader";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { createStyles } from './BaseScreen.styles';
 
 type Props = {
   children: React.ReactNode;
@@ -16,7 +16,7 @@ export function BaseScreen({ children, headerProps }: Props) {
   const styles = useMemo(() => createStyles(theme), [theme]);
 
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={styles.container} edges={["top", "left", "right"]}>
       {headerProps && <ScreenHeader headerTitle={headerProps.headerTitle} />}
       {children}
     </SafeAreaView>
